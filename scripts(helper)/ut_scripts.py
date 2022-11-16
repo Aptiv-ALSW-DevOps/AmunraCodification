1c9b7166e20c7ddef000369b448daae237b3e728
deeksha-gopal <deekshag1998@gmail.com>
2022-11-16 15:48:36 +0100

    First Commit

--- scripts(helper)/ut_scripts.py
+++ scripts(helper)/ut_scripts.py
@@ -45,7 +45,7 @@ def test_convert_string_to_bool_test10():
     assert output is False
 
 def test_convert_string_to_bool_test11():
-    output = convert_string_to_bool(!(type(str)))
+    output = convert_string_to_bool((type(int)))
     assert output is None
 
 
