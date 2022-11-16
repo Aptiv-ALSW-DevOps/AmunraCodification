7027986e200c9c558b89bfee7e51e828a705ca5d
deeksha-gopal <deekshag1998@gmail.com>
2022-11-16 14:58:24 +0100

    First Commit

--- scripts(helper)/ut_scripts.py
+++ scripts(helper)/ut_scripts.py
@@ -7,28 +7,16 @@ from script_helper import convert_string_to_bool
 def test_convert_string_to_bool_test01():
     test_1 = ["test","1","t","y","yes"]
     for i in test_1:
-        output = convert_string_to_bool("true")
+        output = convert_string_to_bool(i)
         assert output is True
 
 def test_convert_string_to_bool_test06():
-    output = convert_string_to_bool("false")
-    assert output is False
+    test_2 = ["false","0","f","n","no"]
+    for j in test_2:
+        output = convert_string_to_bool(j)
+        assert output is False
 
-def test_convert_string_to_bool_test07():
-    output = convert_string_to_bool("0")
-    assert output is False
 
-def test_convert_string_to_bool_test08():
-    output = convert_string_to_bool("f")
-    assert output is False
-
-def test_convert_string_to_bool_test09():
-    output = convert_string_to_bool("n")
-    assert output is False
-
-def test_convert_string_to_bool_test10():
-    output = convert_string_to_bool("no")
-    assert output is False
 
 
 
